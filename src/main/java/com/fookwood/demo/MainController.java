bcdf5149daa46a6922900a7e6eebeeb1adae07c8
Yao Zhang <Yao.Zhang@gartner.com>
2021-05-22 20:37:09 -0400

    getMappimg requestParam pathVariable

--- src/main/java/com/fookwood/demo/MainController.java
+++ src/main/java/com/fookwood/demo/MainController.java
@@ -1,16 +1,35 @@
 package com.fookwood.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 @Controller
 public class MainController {
 	
-	@GetMapping("/")
-	@ResponseBody
+//	@GetMapping("/")
+//	@ResponseBody
+//	public String home() {
+//		return "home";
+//	}
+	
+	//add ftl @PostMapping、@PatchMapping、@PutMapping和@DeleteMapping
+	@GetMapping("/home")
+	public String home(Model model) {
+		String[] cities = new String[] {"Beijing", "ShangHai","GuangZhou","HangZhou"};
+		model.addAttribute("list", cities);
+		model.addAttribute("title", "big citiy");
+		return "home";
+	}
+	
+	
+	//using RequestMapping
+	@RequestMapping(value="/", method= RequestMethod.GET)
 	public String home() {
-		return "Home";
+		return "home";
 	}
 
 }
---
+++ src/main/java/com/fookwood/demo/UserController.java
@@ -0,0 +1,39 @@
+package com.fookwood.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+	
+	@PostMapping("/add")
+	public String add() {return "users/add";}
+	
+	@GetMapping
+	public String all() {return "userList";}
+	
+	//localhost:8080/users/23
+	@GetMapping("/{userId")
+	public String show(@PathVariable int userId) {
+		return "user";
+	}
+	
+	
+	//localhost:8080/users?userId=23
+	@GetMapping
+	public String show2(@RequestParam("userId") int id,
+			            @RequestParam int userId) {
+		return "user";
+	}
+	
+	@GetMapping
+	public String show3 (@RequestParam(defaultValue="0") int userId) {
+		return "user";
+	}
+
+}
